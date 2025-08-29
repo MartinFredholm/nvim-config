ccb24bfc88793f60c55105ed3f2e5d3afe4e55bf
Martin Fredholm <martin313.fredholm@gmail.com>
2025-08-29 13:52:06 +0200

    Added Markdown support, but math is a bit janky

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,5 +1,16 @@
 vim.pack.add({
-    {src = "https://github.com/MeanderingProgrammer/render-markdown.nvim"}
+    { src = "https://github.com/OXY2DEV/markview.nvim" },
 })
 
-require('render-markdown')
+require("markview").setup({
+    preview = {
+        enable        = true,
+        icon_provider = 'mini',
+        modes         = { "c", "n", "no", "i", "v" },
+        hybrid_modes  = { "i", "v" },
+    },
+
+    markdown_inline = {
+        enable = true,
+    },
+})
---
+++ test.md
@@ -0,0 +1,9 @@
+# Header one 
+
+
+This is some text with math inline $x+y = \frac{1}{2}$
+$$
+x = \frac{5}{2}
+$$ 
+
+
